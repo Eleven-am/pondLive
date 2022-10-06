6b91f9ad5d6ea254c9b3ffca46ea4e4b1d61bf2f
Eleven-am <royossai@yahoo.com>
2022-10-06 10:05:28 +0200

    v0.1.83

--- declarations/src/pondbase/basePromise.d.ts
+++ declarations/src/pondbase/basePromise.d.ts
@@ -1,9 +1,8 @@
 import { RejectPromise } from "./types";
-export declare class PondError<T> implements RejectPromise<T> {
+export declare class PondError<T> extends Error implements RejectPromise<T> {
     errorMessage: string;
     errorCode: number;
     data: T;
-    stack?: string;
     constructor(errorMessage: string, errorCode: number, data: T);
 }
 export declare function BasePromise<T, V>(data: V, callback: (resolve: (value: T) => void, reject: (((errorMessage: string, errorCode?: number) => void))) => void): Promise<T>;
